d9cefd79655c208e02a37c3a17c8ede68b7aafbe
mrdennychandra <dennyc.dev@gmail.com>
2017-07-31 15:03:03 +0700

    test

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-$params = json_decode(file_get_contents('php://input'));
+//$params = json_decode(file_get_contents('php://input'));
 $barang = $_POST['barang'];
 $text = 'anda memesan ' . $barang;
 $out = [
     'speech' => $text,
     'displayText' => $text,
     'data' => NULL,
-    'contextOut' => $params,
+    'contextOut' => NULL,
     'source' => 'webhook'];
 header('Content-type: application/json');
 echo json_encode($out);
